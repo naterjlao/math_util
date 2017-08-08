68f6fcabc0c6eb8205aa598785e5fc84428d3f4c
Nate Lao <lao.nathan@yahoo.com>
2017-08-07 22:50:42 -0400

    First Commit

---
+++ trig.rb
@@ -0,0 +1,14 @@
+# Trigonometry Functions
+
+
+def degToRads(degrees)
+	return degrees*Math::PI/180
+end
+
+def sin(degrees) 
+	return Math.sin(degToRads(degrees))
+end
+
+def cos(degrees)
+	return Math.cos(degToRads(degrees))
+end
